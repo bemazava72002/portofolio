b7c3ce65782597451b0fd74310b714045faea98e
bemazava007 <bemazavajulio72002@gmail.com>
2025-07-20 11:46:54 +0300

    v38

--- app/page.tsx
+++ app/page.tsx
@@ -655,7 +655,7 @@ export default function Portfolio() {
                     </div>
                     <div>
                       <p className="font-medium text-slate-900">Email</p>
-                      <p className="text-slate-600">bemazavajulio72002@email.com</p>
+                      <p className="text-slate-600 font-small">bemazavajulio72002@email.com</p>
                     </div>
                   </div>
                   
@@ -665,7 +665,7 @@ export default function Portfolio() {
                     </div>
                     <div>
                       <p className="font-medium text-slate-900">Téléphone</p>
-                      <p className="text-slate-600">+261 32 51 517 78</p>
+                      <p className="text-slate-600 font-small">+261 32 51 517 78</p>
                     </div>
                   </div>
                   
@@ -674,8 +674,8 @@ export default function Portfolio() {
                       <MapPin className="w-6 h-6 text-purple-600" />
                     </div>
                     <div>
-                      <p className="font-medium text-slate-900">Localisation</p>
-                      <p className="text-slate-600">Fianarantsoa, Madagascar</p>
+                      <p className="font-medium text-slate-900 ">Localisation</p>
+                      <p className="text-slate-600 font-small">Fianarantsoa, Madagascar</p>
                     </div>
                   </div>
                 </div>
