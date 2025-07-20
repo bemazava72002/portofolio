5ca958b0c0a36b5fd8b4b5f409e286830fb3284c
bemazava007 <bemazavajulio72002@gmail.com>
2025-07-20 15:54:26 +0300

    v61

--- app/page.tsx
+++ app/page.tsx
@@ -207,7 +207,7 @@ export default function Portfolio() {
         <div className="container mx-auto px-4">
           <div className="max-w-4xl mx-auto text-center">
             <div className="mb-8">
-              <div className="w-32 h-30 border-3 from-blue to-purple mx-auto mb-6 rounded-full flex items-center justify-center">
+              <div className="w-30 h-30 border-3 from-blue to-purple mx-auto mb-6 rounded-full flex items-center justify-center">
                 <img 
           src="./bmz.jpg" 
          
