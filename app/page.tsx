b40953699afa9a1201e9d6087e0f7f2f12a72d04
bemazava007 <bemazavajulio72002@gmail.com>
2025-07-20 10:43:29 +0300

    v12

--- app/page.tsx
+++ app/page.tsx
@@ -655,7 +655,7 @@ export default function Portfolio() {
                     </div>
                     <div>
                       <p className="font-medium text-slate-900">Email</p>
-                      <p className="text-slate-600 font-sm">bemazavajulio72002@email.com</p>
+                      <p className="text-slate-600 font-medium mx-auto">bemazavajulio72002@email.com</p>
                     </div>
                   </div>
                   
