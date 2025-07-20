048bec3f35bbdc6d0da5866e9376e4c388067ac0
bemazava007 <bemazavajulio72002@gmail.com>
2025-07-20 11:39:26 +0300

    v35

--- app/page.tsx
+++ app/page.tsx
@@ -693,11 +693,10 @@ export default function Portfolio() {
                 </div>
               </div>
 
-              
-                
+              <div>
                 <h3 className="text-xl font-bold text-slate-900 mb-6">Envoyez-moi un message</h3>
                 <form className="space-y-6 grid md:grid-cols-2 gap-8 ">
-                  <div className="grid md:grid-cols-2 gap-8  ">
+                  <div className="grid md:grid-cols-2 gap-4">
                     <div>
                       <Label htmlFor="name">Nom</Label>
                       <Input id="name" placeholder="Votre nom" />
